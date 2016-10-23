99ebb53ee74cd6cbe03a46e02d82ddddf54cd013
Lancelot Salavert <lancelot.salavert@gmail.com>
2016-10-23 18:46:24 +0100

    extra attributes created

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,21 @@
+class ProfilesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :show ]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    current_user.update(user_params)
+    redirect_to profile_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :photo)
+  end
+end
---
+++ db/migrate/20161023174347_add_profile_attributes_to_users.rb
@@ -0,0 +1,8 @@
+class AddProfileAttributesToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :profile_picture, :string
+    add_column :users, :level, :string
+  end
+end
