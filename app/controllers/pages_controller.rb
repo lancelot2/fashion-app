6b5a77d5a0fef7f454d361e15e816bdeb97e658d
Alex Benoit <alexander.benoit@outlook.com>
2016-10-30 18:13:30 +0100

    fixed secondary navbar crash when not logged in - changed hide navbar to show navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @disable_nav = true
   end
 
   def product_index
+    @show_nav = true
     # @search = Search.new(search_params)
     # @search.save
     # redirect_to searches_path
