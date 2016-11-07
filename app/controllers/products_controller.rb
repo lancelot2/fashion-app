e4d5348168ba8bbd94fe1b5aa81aa07ba99dbf28
Alex Benoit <alexander.benoit@outlook.com>
2016-11-07 17:06:04 +0000

    changed api backend url and deleted double api client2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,18 +5,6 @@ class ProductsController < ApplicationController
     @tab = 'product_index'
     client = DataApiClient.new
     @items = client.call_all_items
-
-    # ca plante du coup pour le moment:
-    @items = []
-    # @search = Search.new(search_params)
-    # @search.save
-    # redirect_to searches_path
-    # @tab = 'product_index'
-
-    # require 'json'
-    # filepath = 'beers.json'
-    # serialized_products = File.read(filepath)
-    # products = JSON.parse(serialized_products)
   end
 
   def show
--- app/services/data_api_client.rb
+++ app/services/data_api_client.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
-# Client that calls the data api
 class DataApiClient
   def call_all_items
-    base_url = 'https://fashion-app-api.herokuapp.com/api/v1/items'
+    base_url = 'https://smartdata-api.herokuapp.com/v1/items'
     return JSON.parse Curl::Easy.perform(base_url).body_str
   rescue => e
     puts "*** ! Error making API call, error msg: #{e} ! ***"
   end
 
   def call_one_item(id)
-    base_url = 'https://fashion-app-api.herokuapp.com/api/v1/items/'
+    base_url = 'https://smartdata-api.herokuapp.com/v1/items/'
     return JSON.parse Curl::Easy.perform(base_url + id.to_s).body_str
   rescue => e
     puts "*** ! Error making API call, error msg: #{e} ! ***"
