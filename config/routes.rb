9c83ac37c8a168921bd2bf331a2be9d5557cef8d
Alex Benoit <alexander.benoit@outlook.com>
2016-11-08 18:02:09 +0000

    typos here and there & removed the image pixelation

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  scope '(:locale)', locale: /fr|es/ do
+  scope '(:locale)', locale: /fr|en/ do
     get '/users/:id', to: 'profiles#show', as: :profile
     get '/profile/edit', to: 'profiles#edit', as: :edit_profile
     patch '/profile', to: 'profiles#update', as: :update_profile
 
     root to: 'pages#home'
-    get 'home2', to: 'pages#home2', as: :home2
+
     get 'product_index', to: 'products#index', as: :product_index
     get 'product_show/:id', to: 'products#show', as: :product_show
+
     resources :companies
     resources :searches
     resources :templates
